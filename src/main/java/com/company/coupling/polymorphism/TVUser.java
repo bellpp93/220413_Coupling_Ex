45df924f7afebe0d1e8fdb23fadce1a421c96912
JongIn Yoon <bellpp93@gmail.com>
2022-04-14 17:30:41 +0900

    0414_스프링 설정 파일 생성 적용 결합도 예제

--- src/main/java/com/company/coupling/polymorphism/TVUser.java
+++ src/main/java/com/company/coupling/polymorphism/TVUser.java
@@ -1,14 +1,23 @@
 package com.company.coupling.polymorphism;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class TVUser {
 
 	public static void main(String[] args) {
-//		TV tv = new SamsungTV();
-		TV tv = new LgTV();
+		// 1. Spring 컨테이너를 구동한다.
+		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
+		
+		// 2. Spring 컨테이너로 필요한 객체를 요청(Lookup)한다.
+		TV tv = (TV)factory.getBean("tv");
 		
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
 		tv.powerDown();
+		
+		// 3. Spring 컨테이너를 종료한다.
+		factory.close();
 	}
 }
