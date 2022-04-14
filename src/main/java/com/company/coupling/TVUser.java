d4e7012d9e394ae24fec23f31ed17c4b79f8776b
JongIn Yoon <bellpp93@gmail.com>
2022-04-14 14:29:06 +0900

    0414_결합도(Coupling) 예제

--- src/main/java/com/company/coupling/TVUser.java
+++ src/main/java/com/company/coupling/TVUser.java
@@ -1,10 +1,16 @@
 package com.company.coupling;
 
+/*
+ * 실행 클래스 => main() 메소드가 포함된 클래스
+ */
 public class TVUser {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		SamsungTV tv = new SamsungTV();
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
 	}
-
 }
